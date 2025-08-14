0195a266030347cd015843e0cfd9a0a5c90ca59b
uni21x <unilaw@gmx.de>
2025-08-14 21:23:12 +0200

    Update function to take book_path parameter

--- stats.py
+++ stats.py
@@ -9,8 +9,8 @@ def get_number_of_characters(book_path):
             char_counts[character] = char_counts.get(character, 0) + 1
         return char_counts
             
-def get_sorted_list_of_dictionaries(book_name):
-    char_counts = get_number_of_characters(book_name)
+def get_sorted_list_of_dictionaries(book_path):
+    char_counts = get_number_of_characters(book_path)
     sorted_characters = sorted(char_counts.items(), key=lambda item: item[1], reverse=True)
     return sorted_characters
  
